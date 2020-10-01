7b7ec9aecb4cec19bbf021b798a0b2e01cda1955
Brastin <brastin3@gmail.com>
2020-10-01 20:06:32 +1000

    adding some stuff back in

--- sketch.js
+++ sketch.js
@@ -10,28 +10,28 @@ function setup(){
   //canvas.position(0,0);
   //canvas.style("z-index","-1");
   //canvas.elt.style.position = "fixed"
-  //for(var i = 0;i<blobcount;i++){
-  //  shapes.push(new Shape(i));
-  //}
-  //pg = createGraphics(windowWidth/20, windowHeight/20);
+  for(var i = 0;i<blobcount;i++){
+    shapes.push(new Shape(i));
+  }
+  pg = createGraphics(windowWidth/20, windowHeight/20);
   //document.body.style.overflow_x = 'hidden';
 
-  //colorMode(HSB,255,255,255);
+  
 }
 
 function draw(){
   //observer.observe(document.querySelector('nav'));
-  //pg.background(0);
-  //pg.colorMode(RGB);
-  //pg.background(24, 32, 51);
-  //pg.colorMode(HSB);
+  pg.background(0);
+  pg.colorMode(RGB);
+  pg.background(24, 32, 51);
+  pg.colorMode(HSB);
   background(255);
 
-  //for(var i = 0;i<shapes.length;i++){
-  //  shapes[i].show();
-  //}
-  //pg.filter(BLUR,4);
-  //image(pg, 0, 0,windowWidth,height+20);
+  for(var i = 0;i<shapes.length;i++){
+    shapes[i].show();
+  }
+  pg.filter(BLUR,4);
+  image(pg, 0, 0,windowWidth,height+20);
   
 }
 
@@ -41,7 +41,7 @@ function windowResized(){
 
 class Shape {
   constructor(index){
-    
+    colorMode(HSB,255,255,255);
     this.x = random(0,windowWidth/20);
     this.y = random(0,windowHeight/20);
     this.time = random(0,60);
@@ -55,11 +55,11 @@ class Shape {
   }
 
   show(){
-    //pg.colorMode(HSB,255,255);
+    pg.colorMode(HSB,255,255);
     this.time+=0.2;
-    //pg.noStroke();
-    //pg.fill(this.c);
-    //pg.ellipse(this.x,this.y,this.size,this.size);
+    pg.noStroke();
+    pg.fill(this.c);
+    pg.ellipse(this.x,this.y,this.size,this.size);
     this.x+=this.xspeed;
     this.y+=this.yspeed;
     if(this.time>60){
